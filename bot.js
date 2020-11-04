edb7cda57b08c055d5a7eb8ebd981ff6b0cea72c
Michele <barbmich.since86@gmail.com>
2020-11-03 19:50:58 -0500

    npm init, install discord.js and dotenv. create .gitignore. create .env for bot token and other possible needed IDs/secrets/etc. create bot.js with initial code to put bot online.

---
+++ bot.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+require("dotenv").config();
+
+const token = process.env.BOT_TOKEN;
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("message", (msg) => {
+  if (msg.content === "ping") {
+    msg.reply("Pong!");
+  }
+});
+
+client.login(token);
